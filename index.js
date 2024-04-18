27dcce192cf3483e4263b6671d8d4e56d275950a
Hiren Sojitra <hiren3@amnex.com>
2024-04-18 19:26:58 +0530

    Images Added

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const authRouter = require('./routes/auth.router')
 const districtRouter = require('./routes/district.router')
 const talukaRouter = require('./routes/taluka.router')
 const villageRouter = require('./routes/village.router')
+const imagesRouter = require('./routes/images.router')
 
 app.use("/api/v1/posts", postsRouter)
 app.use("/api/v1/post-detail", postDetail)
@@ -21,6 +22,7 @@ app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/district", districtRouter)
 app.use("/api/v1/taluka", talukaRouter)
 app.use("/api/v1/village", villageRouter)
+app.use("/api/v1/images", imagesRouter)
 
 const PORT = process.env.PORT || 5000
 
